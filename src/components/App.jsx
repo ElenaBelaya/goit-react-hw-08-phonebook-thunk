545845dea5298cca6e4ade58fc102e9714f6751f
ElenaBelaya <makseniya@ukr.net>
2022-08-13 22:31:47 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import { useEffect } from 'react';
+import ContactForm from './contactForm';
+import ContactList from './contactList';
+import Section from './section';
+import { Container } from './section/Section.styled';
+import Filter from './filter';
+import { useSelector } from 'react-redux';
+
+const KEY = 'contacts';
+
 export const App = () => {
+  const contacts = useSelector(state => state.contacts.items);
+
+  useEffect(() => {
+    window.localStorage.setItem(KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Section title={'Phonebook'}>
+        <ContactForm />
+      </Section>
+      <Section title={'Contacts'}>
+        <Filter />
+        <ContactList />
+      </Section>
+    </Container>
   );
 };
