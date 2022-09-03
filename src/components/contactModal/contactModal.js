85eaa68dda3f396b24302a0070575b562e3636e7
ElenaBelaya <makseniya@ukr.net>
2022-09-04 01:01:25 +0300

    update

--- src/components/contactModal/contactModal.js
+++ src/components/contactModal/contactModal.js
@@ -15,7 +15,6 @@ const editContact = () => {
           <label htmlFor={nameId}>
             <TitleInput>Name</TitleInput>
             <FieldStyled
-              id={nameId}
               type="text"
               name="name"
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
@@ -26,7 +25,6 @@ const editContact = () => {
           <label htmlFor={phoneId}>
             <TitleInput>Phone</TitleInput>
             <FieldStyled
-              id={phoneId}
               type="text"
               name="number"
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
